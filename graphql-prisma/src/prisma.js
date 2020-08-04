927edfcd10845729ba0ec54c64e19e40063ee895
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2020-08-04 19:50:26 -0300

    Add graphql-prisma

---
+++ graphql-prisma/src/prisma.js
@@ -0,0 +1,9 @@
+import { Prisma } from 'prisma-binding';
+
+const prisma = new Prisma({
+	typeDefs: 'src/generated/prisma.graphql',
+	endpoint: 'http://localhost:4466',
+	secret: 'testsecret'
+});
+
+export { prisma as default };
---
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -0,0 +1,102 @@
+const Mutation = {
+	async createUser(parent, args, { prisma }, info) {
+		const emailTaken = await prisma.exists.User({ email: args.data.email });
+
+		if (emailTaken) {
+			throw new Error('Email taken');
+		}
+
+		return prisma.mutation.createUser({ data: args.data }, info);
+	},
+
+	async deleteUser(parent, args, { prisma }, info) {
+		const userExists = await prisma.exists.User({ id: args.id });
+
+		if (!userExists) {
+			throw new Error('User not found');
+		}
+
+		return prisma.mutation.deleteUser({
+			where: {
+				id: args.id,
+			},
+		}, info);
+	},
+
+	async updateUser(parent, args, { prisma }, info) {
+		return prisma.mutation.updateUser({
+			where: {
+				id: args.id,
+			},
+			data: args.data,
+		}, info);
+	},
+
+	async createPost(parent, args, { prisma }, info) {
+		return prisma.mutation.createPost({
+			data: {
+				...args.data,
+				author: {
+					connect: {
+						id: data.author,
+					},
+				},
+			},
+		}, info);
+	},
+
+	async deletePost(parent, args, { prisma }, info) {
+		return prisma.mutation.deletePost({
+			where: {
+				id: args.id,
+			},
+		}, info);
+	},
+
+	async updatePost(parent, args, { prisma }, info) {
+		return prisma.mutation.updatePost({
+			where: {
+				id: args.id,
+			},
+			data: args.data,
+		}, info);
+	},
+
+	async createComment(parent, args, { prisma }, info) {
+		return prisma.mutation.createComment({
+			data: {
+				text: args.data.text,
+				author: {
+					connect: {
+						id: args.data.author,
+					},
+				},
+				post: {
+					connect: {
+						id: args.data.post,
+					},
+				},
+
+			}
+		}, info)
+	},
+
+	async deleteComment(parent, args, { prisma }, info) {
+		return prisma.mutation.deleteComment({
+			where: {
+				id: args.id,
+			},
+		}, info);
+	},
+
+	async updateComment(parent, args, { prisma }, info) {
+		return prisma.mutation.updateComment({
+			where: {
+				id: args.id,
+			},
+			data: args.data,
+		}, info);
+	},
+};
+
+export { Mutation as default };
