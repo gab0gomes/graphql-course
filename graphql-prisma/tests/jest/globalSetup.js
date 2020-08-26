eec5ac1c4ad2f2226e00ba64d5401199de9a721f
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2020-08-25 21:40:40 -0300

    Finaliza setup de testes

---
+++ graphql-prisma/tests/jest/globalSetup.js
@@ -0,0 +1,12 @@
+// require('babel-register');
+// require('@babel/polyfill/noConflict');
+require("core-js/stable");
+require("regenerator-runtime/runtime");
+
+const server = require('../../src/server').default;
+
+module.exports = async () => {
+	global.httpServer = await server.start({
+		port: 4000,
+	});
+}
